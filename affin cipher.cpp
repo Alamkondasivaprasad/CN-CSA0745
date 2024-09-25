2bbe62ed65497591c201117ad38b81ee45c0c6ac
Alamkondasivaprasad <128775990+Alamkondasivaprasad@users.noreply.github.com>
2024-09-25 21:24:41 +0530

    Add files via upload

---
+++ affin cipher.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    char ciphertext[] = "BUBUBUB";  
+    char mostFrequent = 'BUBB', secondFrequent = 'U'; 
+    printf("Most frequent letter '%c' might be 'E'.\n", mostFrequent);
+    return 0;
+}
+
